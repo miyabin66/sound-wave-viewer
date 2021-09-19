85b9c58b92019721dea05a5dc193b4e97bbef81e
miyabin66 <freezemark5166@gmail.com>
2021-09-19 21:51:04 +0900

    hooks用意

---
+++ hooks/useIndex.ts
@@ -0,0 +1,5 @@
+const useIndex = () => {
+  return {}
+}
+
+export default useIndex
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 import {IndexContextProvider} from '~/context/IndexContext'
 import DefaultLayout from "~/layout/DefaultLayout";
+import {css} from "@emotion/react";
+import useIndex from '~/hooks/useIndex'
+
+const indexContainer = css`
+  position: relative;
+`
+
+const canvasStyle = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+`
 
 const Index = () => {
+  const {} = useIndex()
   return (
     <IndexContextProvider>
       <DefaultLayout>
-        <p>ok</p>
+        <div css={indexContainer}>
+          <canvas css={canvasStyle} />
+        </div>
       </DefaultLayout>
     </IndexContextProvider>
   )
