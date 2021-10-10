eafe5a4b6b4183a900169486d0adb037c8afdc05
miyabin66 <freezemark5166@gmail.com>
2021-10-10 22:55:03 +0900

    shader実装

--- components/hooks/useIndex.ts
+++ components/hooks/useIndex.ts
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { Mesh, MeshBasicMaterial, OrthographicCamera, PlaneGeometry, Scene, WebGLRenderer } from 'three';
+import { Mesh, OrthographicCamera, PlaneGeometry, Scene, ShaderMaterial, Vector3, WebGLRenderer } from 'three';
 
 const useIndex = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -8,6 +8,22 @@ const useIndex = () => {
     const width = window.innerWidth;
     const height = window.innerHeight;
     renderer.setSize(width, height);
+  };
+
+  const fragmentShader = `
+    uniform vec3 iResolution;
+    uniform float iTime;
+    
+    void main() {
+      vec2 uv = gl_FragCoord.xy / iResolution.xy;
+      vec3 col = 0.5 + 0.5 * cos(iTime + uv.xyy + vec3(0, 2, 4));
+      gl_FragColor = vec4(col, 1.0);
+    }
+  `;
+
+  const uniforms = {
+    iTime: { value: 0 },
+    iResolution:  { value: new Vector3() },
   };
 
   useEffect(() => {
@@ -28,14 +44,18 @@ const useIndex = () => {
     );
     const scene = new Scene();
     const plane = new PlaneGeometry(2, 2);
-    const material = new MeshBasicMaterial({
-        color: 'red',
+    const material = new ShaderMaterial({
+      fragmentShader,
+      uniforms,
     });
     scene.add(new Mesh(plane, material));
 
     renderer.setSize(width, height);
     
-    const render = () => {
+    const render = (time: number) => {
+      time *= 0.001;
+      uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
+      uniforms.iTime.value = time;
       renderer.render(scene, camera);
       requestAnimationFrame(render);
     };
